c00f7f2aa483026a793825ac8d5138633412fa79
Riyad Hossain <rh005323@gmail.com>
2024-09-04 13:17:46 +0800

    add code

--- Asp.Net/FoodDelievery/FastFood.Web/Controllers/HomeController.cs
+++ Asp.Net/FoodDelievery/FastFood.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using FastFood.Web.Models;
+﻿using FastFood.Models;
+using FastFood.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace FastFood.Web.Controllers
@@ -7,26 +10,43 @@ namespace FastFood.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private object _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            ItemListViewModel vm = new ItemListViewModel()
+            {
+                Items = await _context.Items.Include(x => x.Category).Include(y => y.SubCategory),
+                Categories = await _context.Categories.ToListAsync(),
+            Coupons = await _context.Coupons.Where(c => c.IsActive==true).ToListAsync()
+            };
 
+            return View(vm);
+        }
+           
         public IActionResult Privacy()
+    {
+        return View();
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    }
+
+    internal class ItemListViewModel
+    {
+        public ItemListViewModel()
         {
-            return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public object Items { get; set; }
+        public object Categories { get; set; }
+        public object Coupons { get; set; }
     }
 }
